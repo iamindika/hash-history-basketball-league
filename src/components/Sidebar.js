9d0c159e0984ee5b8aa7a9fc9543fee2279075bf
iamindika <ikolamba2@gmail.com>
2022-04-15 09:28:21 -0400

    create Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import slug from 'slug';
+
+Sidebar.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+}
+
+function CustomLink({to, children}){
+  return (
+    <Route 
+      path={to.pathname} 
+      children={({match}) => (
+        <li style={{"listStyleType": "none", "fontWeight": match ? "bold" : "normal"}}>
+          <Link to={to}>{children}</Link>
+        </li>
+      )}
+    />
+  )
+}
+
+export default function Sidebar({title, list, loading, location, match}) {
+  return loading === true
+    ? <h2>Loading</h2>
+    : <div>
+        <h3 className="header">{title}</h3>
+        
+        <ul className="sidebar-list">
+          {list.map((item) => (
+            <CustomLink
+              key={item}
+              to={{
+                pathname: `${match.url}/${slug(item)}`,
+                search: location.search
+              }}
+            >{item.toUpperCase()}</CustomLink>
+          ))}
+        </ul>
+      </div>
+}
