5f3a0b7e0fe4aac22b3ec4282ad58c59abf8b564
iamindika <ikolamba2@gmail.com>
2022-04-15 13:13:57 -0400

    add Route for TeamPage component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
+import TeamPage from './TeamPage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/players" component={Players} />
           <Route path="/teams" component={Teams} />
+          <Route path="/:teamId" component={TeamPage} />
           <Route render={({location}) => (
             <h2 className="text-center">
               404: {location.pathname} not found!
